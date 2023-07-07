345dd028c7790dd2a2f46ee253458681468c679f
Akshat23x3 <akshatkhare69@gmail.com>
2023-07-07 18:04:56 +0530

    build v0.1.3

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import styles from "./NavigationBar.module.css"
 import logo from "./Extras/logo.png"
+import {useNavigate} from "react-router-dom"
 
 export const NavigationBar = () =>{
-
+const history = useNavigate()
 
 return(
 <nav className="navigationBar">
-<a href="/Samsung">
+<a href="/Samsung" onClick={history("/Samsung")}>
  <img className={styles.companyLogo} src={logo} alt="Company Logo" />
  </a>
 <ul>
-<li> <a href="/Samsung/Shop"> Shop </a> </li>
-<li> <a href="/Samsung/Mobile"> Mobile </a> </li>
-<li> <a href="/Samsung/TVAAV"> TV & AV </a> </li>
-<li> <a href="/Samsung/HomeAppliances"> Home Appliances </a> </li>
-<li> <a href="/Samsung/Laptops"> Laptops </a> </li>
-<li> <a href="/Samsung/Displays"> Displays </a> </li>
+<li> <a href="/Shop" onClick={history("/Shop")}> Shop </a> </li>
+<li> <a href="/Mobile" onClick={history("/Mobile")}> Mobile </a> </li>
+<li> <a href="/TVAAV" onClick={history("/TVAAV")}> TV & AV </a> </li>
+<li> <a href="/HomeAppliances" onClick={history("/HomeAppliances")}> Home Appliances </a> </li>
+<li> <a href="/Laptops" onClick={history("/Laptops")}> Laptops </a> </li>
+<li> <a href="/Displays" onClick={history("/Displays")}> Displays </a> </li>
 </ul>
 </nav>
 
